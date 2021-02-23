ff167e8db9d8effac959d13f9f296d0f70702efe
Mike <mmatthews@manzalab.com>
2021-02-23 17:35:53 +0100

    Setup GIVEN and WHEN parts of integration test

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,11 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
 import org.junit.jupiter.api.AfterAll;
@@ -49,9 +51,18 @@ public class ParkingDataBaseIT {
 
     @Test
     public void testParkingACar(){
+		// GIVEN
+		int parkingNumber = parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR);
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+
+		// WHEN
+		when(parkingService.getNextParkingNumberIfAvailable()).thenReturn(new ParkingSpot(parkingNumber, ParkingType.CAR, true));
         parkingService.processIncomingVehicle();
+
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+
+		// THEN
+		
     }
 
     @Test
