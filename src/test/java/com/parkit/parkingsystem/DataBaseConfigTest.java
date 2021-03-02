3332137c572e6a8bf67497581a8abf07d1c1227f
Mike <mmatthews@manzalab.com>
2021-03-02 14:55:59 +0100

    Added getConnection test

--- src/test/java/com/parkit/parkingsystem/DataBaseConfigTest.java
+++ src/test/java/com/parkit/parkingsystem/DataBaseConfigTest.java
@@ -1,6 +1,15 @@
 package com.parkit.parkingsystem;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
 import com.parkit.parkingsystem.config.DataBaseConfig;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Class used to unit test DataBaseConfig
@@ -11,4 +20,50 @@ import com.parkit.parkingsystem.config.DataBaseConfig;
 public class DataBaseConfigTest
 {
 	static DataBaseConfig dataBaseConfig = new DataBaseConfig();
+
+	/**
+	 * Tests DataBaseConfig.getConnection
+	 * 
+	 * @see com.parkit.parkingsystem.config.DataBaseConfig#getConnection()
+	 */
+	@Test
+	public void testGetConnection()
+	{
+		// GIVEN
+		String expectedDatabaseUrl = "jdbc:mysql://localhost:3306/prod?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
+		boolean expectedConnectionClosedState = false;
+
+		// WHEN
+		Connection testConnection = null;
+
+		try
+		{
+			testConnection = dataBaseConfig.getConnection();
+		}
+		catch (ClassNotFoundException e)
+		{
+			System.out.println(e);
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e);
+		}
+
+		// THEN
+		if (testConnection == null)
+			fail("Couldn't retrieve connection");
+
+		try
+		{
+			assertEquals(expectedConnectionClosedState, testConnection.isClosed());
+
+			DatabaseMetaData metaData = testConnection.getMetaData();
+
+			assertEquals(expectedDatabaseUrl, metaData.getURL());
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e);
+		}
+	}
 }
