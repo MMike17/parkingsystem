d2afa1b0084fed81e5a5a2ae0d19d6bc4fcd48e6
Mike <mmatthews@manzalab.com>
2021-03-02 15:17:57 +0100

    Added test setup

--- src/test/java/com/parkit/parkingsystem/TicketDAOTest.java
+++ src/test/java/com/parkit/parkingsystem/TicketDAOTest.java
@@ -1,6 +1,9 @@
 package com.parkit.parkingsystem;
 
 import com.parkit.parkingsystem.dao.TicketDAO;
+import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
+
+import org.junit.jupiter.api.BeforeAll;
 
 /**
  * Class used to unit test TicketDAO
@@ -10,5 +13,18 @@ import com.parkit.parkingsystem.dao.TicketDAO;
  */
 public class TicketDAOTest
 {
-	static TicketDAO ticketDAO = new TicketDAO();
+	static TicketDAO testTicketDAO = new TicketDAO();
+	static DataBaseTestConfig testDataBase = new DataBaseTestConfig();
+
+	@BeforeAll
+	public static void setupTests()
+	{
+		testTicketDAO.dataBaseConfig = testDataBase;
+	}
+
+	// what methods do we need to test ?
+
+	// boolean saveTicket(Ticket ticket)
+	// Ticket getTicket(String vehicleRegNumber)
+	// boolean updateTicket(Ticket ticket)
 }
