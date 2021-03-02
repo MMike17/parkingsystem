1b23cf57c7f367e7ee39004cf61f965f90adb6c0
Mike <mmatthews@manzalab.com>
2021-03-02 14:54:25 +0100

    Added class to test DataBaseConfig

---
+++ src/test/java/com/parkit/parkingsystem/DataBaseConfigTest.java
@@ -0,0 +1,14 @@
+package com.parkit.parkingsystem;
+
+import com.parkit.parkingsystem.config.DataBaseConfig;
+
+/**
+ * Class used to unit test DataBaseConfig
+ * 
+ * @see com.parkit.parkingsystem.config.DataBaseConfig
+ * @author Mike Matthews
+ */
+public class DataBaseConfigTest
+{
+	static DataBaseConfig dataBaseConfig = new DataBaseConfig();
+}
