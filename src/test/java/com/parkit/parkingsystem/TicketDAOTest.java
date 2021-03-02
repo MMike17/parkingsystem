3e21b7cc8ef8f89a33c2f7979f87e25cc2149481
Mike <mmatthews@manzalab.com>
2021-03-02 15:12:56 +0100

    Added class to test TicketDAO

---
+++ src/test/java/com/parkit/parkingsystem/TicketDAOTest.java
@@ -0,0 +1,14 @@
+package com.parkit.parkingsystem;
+
+import com.parkit.parkingsystem.dao.TicketDAO;
+
+/**
+ * Class used to unit test TicketDAO
+ * 
+ * @see com.parkit.parkingsystem.dao.TicketDAO
+ * @author Mike Matthews
+ */
+public class TicketDAOTest
+{
+	static TicketDAO ticketDAO = new TicketDAO();
+}
