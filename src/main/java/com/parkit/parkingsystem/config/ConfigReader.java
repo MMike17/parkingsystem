4473ca1caa0199f128084b0f709f3d0a53fcb5e8
Mike <mmatthews@manzalab.com>
2021-03-16 17:16:33 +0100

    Specified charset for FileReader instance

--- src/main/java/com/parkit/parkingsystem/config/ConfigReader.java
+++ src/main/java/com/parkit/parkingsystem/config/ConfigReader.java
@@ -1,8 +1,14 @@
 package com.parkit.parkingsystem.config;
 
+import java.io.FileInputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+
+import com.google.protobuf.Descriptors.FileDescriptor;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -30,15 +36,18 @@ public class ConfigReader
 	{
 		JSONParser parser = new JSONParser();
 
-		try(FileReader reader = new FileReader(configFilePath))
+		try
 		{
+			InputStream inputStream = new FileInputStream(configFilePath);
+			Reader reader = new InputStreamReader(inputStream, "UTF-8");
+
 			Object fileContent = parser.parse(reader);
 			JSONObject jsonObject = (JSONObject) fileContent;
 
 			userName = (String) jsonObject.get(userJSONKey);
 			password = (String) jsonObject.get(passwordJSONKey);
 		}
-		catch(IOException | ParseException exception)
+		catch(Exception exception)
 		{
 			System.out.println("Couldn't read config from file\n" + exception);
 		}
