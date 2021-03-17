5124c984e1aaf2c6b7762ab57e20622240d519fb
Mike <mmatthews@manzalab.com>
2021-03-17 14:43:16 +0100

    Revert "Solved possible memory leak in saveTicket"
    This reverts commit 2a7f32c37971bb7a7703ee4e51199bd661bca0a6.

--- src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
+++ src/main/java/com/parkit/parkingsystem/dao/TicketDAO.java
@@ -19,36 +19,24 @@ public class TicketDAO {
 
     public DataBaseConfig dataBaseConfig = new DataBaseConfig();
 
-    public boolean saveTicket(Ticket ticket)
-	{
+    public boolean saveTicket(Ticket ticket){
         Connection con = null;
-		PreparedStatement ps = null;
-		boolean isResultSet = false;
-
-        try
-		{
+        try {
             con = dataBaseConfig.getConnection();
-            ps = con.prepareStatement(DBConstants.SAVE_TICKET);
+            PreparedStatement ps = con.prepareStatement(DBConstants.SAVE_TICKET);
             //ID, PARKING_NUMBER, VEHICLE_REG_NUMBER, PRICE, IN_TIME, OUT_TIME)
             //ps.setInt(1,ticket.getId());
             ps.setInt(1,ticket.getParkingSpot().getId());
             ps.setString(2, ticket.getVehicleRegNumber());
             ps.setDouble(3, ticket.getPrice());
             ps.setTimestamp(4, new Timestamp(ticket.getInTime().getTime()));
-            ps.setTimestamp(5, (ticket.getOutTime() == null)?null: (new Timestamp(ticket.getOutTime().getTime())));
-
-			isResultSet = ps.execute();
-        }
-		catch (Exception ex)
-		{
+            ps.setTimestamp(5, (ticket.getOutTime() == null)?null: (new Timestamp(ticket.getOutTime().getTime())) );
+            return ps.execute();
+        }catch (Exception ex){
             logger.error("Error fetching next available slot",ex);
-        }
-		finally
-		{
+        }finally {
             dataBaseConfig.closeConnection(con);
-			dataBaseConfig.closePreparedStatement(ps);
-
-            return isResultSet;
+            return false;
         }
     }
 
